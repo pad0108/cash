5c9a4e77e9d84dcbebc185f42187f4cb0119ecdb
pad0108 <chkc78@naver.com>
2020-11-02 17:16:01 +0900

    2020-11-02 오후 5:15 년도 추가 이전,다음 달 이동 기능 구현

--- cash/src/main/java/kr/co/gdu/cash/controller/IndexController.java
+++ cash/src/main/java/kr/co/gdu/cash/controller/IndexController.java
@@ -1,5 +1,6 @@
 package kr.co.gdu.cash.controller;
 
+
 import java.util.Calendar;
 import java.util.List;
 
@@ -7,45 +8,64 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.co.gdu.cash.service.IndexService;
 import kr.co.gdu.cash.vo.Notice;
+
 @Controller
 public class IndexController {
-	@Autowired
-	private IndexService indexService;
-	
-	@GetMapping(value={"/","/index"})
-	public String index(Model model) {
-		//1. 요청 분석
-		Calendar c = Calendar.getInstance(); //오늘 날짜 구하기
-		//알고리즘 테스트용
-		//c.set(Calendar.MONTH, 11);
-		
-		int month = c.get(Calendar.MONTH) + 1; // 월
-		int lastDay = c.getActualMaximum(Calendar.DATE);
-		//1일로 만드는 코드
-		Calendar firstDay = Calendar.getInstance();
-		//알고리즘 테스트용
-		//firstDay.set(Calendar.MONTH, 11);
-		
-		firstDay.set(Calendar.DATE, 1);
-		int firstDayOfWeek = firstDay.get(Calendar.DAY_OF_WEEK);
-		//디버깅
-		System.out.println(month+"<-month");
-		System.out.println(lastDay+"<-lastDay");
-		System.out.println(firstDayOfWeek+"<-firstDayOfWeek");
-		//2. 서비스 호출
-		List<Notice> list = indexService.getLatestNoticeList();
-		//디버깅
-		System.out.println("list.size(5) : " + (list.size() == 5));	// true
-		
-		//3.뷰 모델 추가
-		model.addAttribute("month",month); //월
-		model.addAttribute("lastDay",lastDay); // 마지막 일
-		model.addAttribute("firstDayOfWeek",firstDayOfWeek); // 이번달 1일
-		model.addAttribute("list", list);
-		// 실제로 포워딩 되는 것은 prefix에서 지정한 /WEB-INF/view/index.jsp으로 반환됨
-		return "index";	
-	}
+   @Autowired
+   private IndexService indexService;
+
+   @GetMapping(value = { "/", "/index" })
+   //requestparam으로 paramMonth가 null이면 0으로 바꿔라(int로 형변환을 해야하기 떄문에) = ("request.getParamater("paramMonth");) 
+   public String index(Model model,@RequestParam(name = "currentYear",defaultValue = "-1")int currentYear,@RequestParam(name="currentMonth",defaultValue = "-1") int currentMonth) {
+      
+      //1. 요청분석
+      /*
+       *
+       */
+      Calendar currentDay = Calendar.getInstance();// 2020년 11월 2일.
+      //currentYear 넘어오고, currentMonth도  넘어오면 
+      if(currentYear != -1 && currentMonth != -1) {
+         if(currentMonth == 0) {
+            currentMonth = 12; //currentMonth가 0이면 12월로 바뀌고
+            currentYear -=1;  //년도가 1년 빠짐.
+         }
+         if(currentMonth == 13) {//month가 13월 되면
+            currentMonth = 1;  // 1월로 바뀌고
+            currentYear +=1;  // 년도는 1년 올라간다.
+         }
+         currentDay.set(Calendar.YEAR, currentYear);
+         currentDay.set(Calendar.MONTH, currentMonth-1);
+      }
+      currentDay.set(Calendar.DATE, 1); // 2020년 11월1일
+      int year = currentDay.get(Calendar.YEAR);
+      int month= currentDay.get(Calendar.MONTH) + 1; //월
+      int lastDay = currentDay.getActualMaximum(Calendar.DATE);
+      int firstDayOfWeek = currentDay.get(Calendar.DAY_OF_WEEK);
+      
+      //디버깅
+      System.out.println(month+"<--month");
+      System.out.println(lastDay+"<--lastDay");
+      
+   
+      
+      //2. 서비스 호출
+      List<Notice> list = indexService.getLatestNoticeList();
+      //System.out.println("list.size(5)-->" + (list.size() == 5));
+      
+      // 3. 뷰 연결(모델 추가)
+      /*
+       * 1) 월, 마지막 일(30 or 31 , 윤달이 껴서 ..29일인지...), 1일 요일
+       * 공지사항 목록
+       */
+      model.addAttribute("year",year); //년
+      model.addAttribute("month", month);//월
+      model.addAttribute("lastDay",lastDay);//마지막 일
+      model.addAttribute("firstDayOfWeek",firstDayOfWeek); //1일의 요일
+      model.addAttribute("list", list);
+      return "index";
+   }
 }
--- cash/src/main/webapp/WEB-INF/view/index.jsp
+++ cash/src/main/webapp/WEB-INF/view/index.jsp
@@ -1,39 +1,49 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
-	<head>
-	<meta charset="UTF-8">
-	<title>index.jsp</title>
-	</head>
-	<body>
-		<h1>index.jsp</h1>
-		
-		<h3>공지사항</h3>
+<head>
+<meta charset="UTF-8">
+<title>index.jsp</title>
+<style type="text/css">
+.sunday {
+	color: #FF0000;
+}
+</style>
+</head>
+<body>
+	<h1>index.jsp</h1>
+
+	<h3>공지사항</h3>
+	<table border="1">
+		<thead>
+			<tr>
+				<th>notice_id</th>
+				<th>notice_title</th>
+				<th>notice_date</th>
+			</tr>
+		</thead>
+		<tbody>
+			<c:forEach var="notice" items="${list}">
+				<tr>
+					<td>${notice.noticeId}</td>
+					<td>${notice.noticeTitle}</td>
+					<td>${notice.noticeDate}</td>
+				</tr>
+			</c:forEach>
+		</tbody>
+	</table>
+	<!-- 다이어리 -->
+	<h3>
+		<a href="/index?currentYear=${year}&currentMonth=${month-1}">[이전
+			달]</a> ${year}년 ${month}월 <a
+			href="/index?currentYear=${year}&currentMonth=${month+1}">[다음달 달]</a>
+	</h3>
+	<div>
 		<table border="1">
 			<thead>
 				<tr>
-					<th>notice_id</th>
-					<th>notice_title</th>
-					<th>notice_date</th>
-				</tr>
-			</thead>
-			<tbody>
-				<c:forEach var="notice" items="${list}">
-					<tr>
-						<td>${notice.noticeId}</td>
-						<td>${notice.noticeTitle}</td>
-						<td>${notice.noticeDate}</td>
-					</tr>
-				</c:forEach>
-			</tbody>
-		</table>
-		<!-- 다이어리 -->
-		<h3>${month}월</h3>
-		<div>
-			<table border="1" width="100%">
-				<thead>
-					<tr>
 					<th>일</th>
 					<th>월</th>
 					<th>화</th>
@@ -41,28 +51,38 @@
 					<th>목</th>
 					<th>금</th>
 					<th>토</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<c:forEach var ="i" begin="1" end="${lastDay+(firstDayOfWeek-1)}" step="1">					
-							<c:if test="${i-(firstDayOfWeek-1) < 1}">
-								<td>&nbsp;</td>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<c:forEach var="i" begin="1" end="${lastDay+(firstDayOfWeek-1)}"
+						step="1">
+						<c:if test="${i-(firstDayOfWeek-1) < 1}">
+							<td>&nbsp;</td>
+						</c:if>
+						<c:if test="${i-(firstDayOfWeek-1) > 0}">
+							<c:if test="${i%7 == 1}">
+								<td class="sunday">${i-(firstDayOfWeek-1)}</td>
 							</c:if>
-							<c:if test="${i-(firstDayOfWeek-1) > 0}">
-							<td>${i-firstDayOfWeek+1}</td>
+							<c:if test="${i%7 !=1 }">
+								<td>${i-(firstDayOfWeek-1)}</td>
 							</c:if>
-							<c:if test="${i%7 == 0}">
-							</tr><tr>
-							</c:if>
-						</c:forEach>
-						
-						<c:forEach begin="1" end="${7 - ((lastDay+(firstDayOfWeek-1)) % 7)}" step="1">
+						</c:if>
+						<c:if test="${i%7 == 0}">
+				</tr>
+				<tr>
+					</c:if>
+					</c:forEach>
+
+					<c:if test="${(lastDay+(firstDayOfWeek-1)) % 7 != 0}">
+						<c:forEach begin="1"
+							end="${7- ((lastDay+(firstDayOfWeek-1)) % 7)}" step="1">
 							<td>&nbsp;</td>
 						</c:forEach>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	</body>
+					</c:if>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</body>
 </html>
